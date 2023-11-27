3d6e052b9415b15b4058e5186cd9aef556bbb540
Federico Poggi <federicopoggi3@gmail.com>
2023-11-27 19:50:23 +0100

    main3 invertedOrder

--- src/Main2.java
+++ src/Main2.java
@@ -3,35 +3,33 @@ import java.util.Scanner;
 
 public class Main2 {
 
-    static void pariDispari (long number1){
-        long result=number1 % 2;
-        if (result==0){
-            System.out.println("Il tuo numero è Pari");
-        }else {
-            System.out.println("Il tuo numero è Dispari");
-        }
-    }
-
     static String[] alContrario(String[] parole){
         String[] nuovoArrayParole;
-        String parola2="";
-        nuovoArrayParole=new String[3];
-        for (int i = 0; i<parole.length; i++){
-            String indexValue=parole[i];
-            nuovoArrayParole[i]=indexValue;
-            System.out.println(Arrays.toString(nuovoArrayParole));
+        nuovoArrayParole=new String[parole.length];
+
+
+//uso indici per invertire l'ordine e ottener un array invertito;
+
+        for (int i = parole.length-1; i >= 0; i--) {
+            nuovoArrayParole[parole.length-1 - i]=parole[i];
         }
         return nuovoArrayParole;
-    }
+    };
     public static void main(String[] args){
         Scanner scanner= new Scanner(System.in);
 
         {
-            long oddEven;
-            System.out.println("Inserisci un numero e ti dirò se è pari o dispari");
-            oddEven= Integer.parseInt(scanner.nextLine());
-            pariDispari(oddEven);
-
+            String[] arrayParole;
+            arrayParole=new String[3];
+            System.out.println("Inserisci tre parole");
+            for(int i = 0; i<arrayParole.length; i++){
+                String parola;
+                parola= scanner.nextLine();
+                arrayParole[i]=parola;
+            }
+            System.out.println( " Array di Parole:" + " " + Arrays.toString(arrayParole));
+            System.out.println(Arrays.toString(alContrario(arrayParole)));
         }
+
     }
 }
--- src/Main3.java
+++ src/Main3.java
@@ -3,32 +3,36 @@ import java.util.Scanner;
 
 public class Main3 {
 
-    static String[] alContrario(String[] parole){
-        String[] nuovoArrayParole;
-        String parola2="";
-        nuovoArrayParole=new String[3];
-        for (int i = 0; i<parole.length; i++){
-            String indexValue=parole[i];
-            nuovoArrayParole[i]=indexValue;
+    static void pariDispari(long number1) {
+        long result = number1 % 2;
+        if (result == 0) {
+            System.out.println("Il tuo numero è Pari");
+        } else {
+            System.out.println("Il tuo numero è Dispari");
+        }
+    }
 
+    static String[] alContrario(String[] parole) {
+        String[] nuovoArrayParole;
+        String parola2 = "";
+        nuovoArrayParole = new String[3];
+        for (int i = 0; i < parole.length; i++) {
+            String indexValue = parole[i];
+            nuovoArrayParole[i] = indexValue;
+            System.out.println(Arrays.toString(nuovoArrayParole));
         }
         return nuovoArrayParole;
     }
-    public static void main(String[] args){
-        Scanner scanner= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         {
-            String[] arrayParole;
-            arrayParole=new String[3];
-            System.out.println("Inserisci tre parole");
-            for(int i = 0; i<arrayParole.length; i++){
-                String parola;
-                parola= scanner.nextLine();
-                arrayParole[i]=parola;
-            }
-            System.out.println( " Array di Parole:" + " " + Arrays.toString(arrayParole));
-           System.out.println(Arrays.toString(alContrario(arrayParole)));
-        }
+            long oddEven;
+            System.out.println("Inserisci un numero e ti dirò se è pari o dispari");
+            oddEven = Integer.parseInt(scanner.nextLine());
+            pariDispari(oddEven);
 
+        }
     }
 }
